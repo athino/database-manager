21a3a421aab8c02b4922d9d408130567c6279daf
Johannes Skuterud <johann1301s@icloud.com>
2022-05-16 22:17:43 +0200

    w

--- src/cluster/createpackage/utils/createShasum.ts
+++ src/cluster/createpackage/utils/createShasum.ts
@@ -1,6 +1,7 @@
 import {Connection} from 'common/external/database'
-import crypto from 'crypto'
 import {getFile} from './getFile'
+import ssri from 'ssri'
+import {stream2buffer} from './streamToBuffer'
 
 export const createShasum = async (arg: {
   connection: Connection
@@ -8,29 +9,30 @@ export const createShasum = async (arg: {
   bucketName: string
   fileId: string
 }): Promise<{ shasum: undefined | string }> => {
-  const hash = crypto.createHash('sha1')
-  hash.setEncoding('hex')
 
-  const stream = await getFile({
+  const tarball = await getFile({
     connection: arg.connection,
     databaseName: arg.databaseName,
     bucketName: arg.bucketName,
     fileId: arg.fileId
   })
 
-  return new Promise((resolve, reject) => {
-    stream.on('error', () => {
-      reject({
-        shasum: undefined
-      })
-    })
 
-    stream.on('end', () => {
-      resolve({
-        shasum: hash.end().read()
-      })
-    })
+  const buffer = await stream2buffer(tarball)
 
-    stream.pipe(hash)
+  const integrity = ssri.fromData(buffer, {
+    algorithms: ['sha1', 'sha512']
   })
+
+  const shasum = integrity.hexDigest()
+
+  console.log(shasum)
+
+  //const shasum = integrity?.sha1?.[0]?.hexDigest()
+  
+  // integrity: ssri.parse(integrity.sha512[0]),
+
+  return {
+    shasum
+  }
 }
