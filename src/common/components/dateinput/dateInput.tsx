ddf67de36525ca156e7864b66da9f169d9907ff7
Johannes <johann1301h@icloud.com>
2022-05-30 23:06:16 +0200

    w

--- src/common/components/dateinput/dateInput.tsx
+++ src/common/components/dateinput/dateInput.tsx
@@ -1,16 +1,78 @@
-import React, {FC, useRef} from 'react'
+import React, {FC, useRef, useState} from 'react'
 import styled from 'styled-components'
 
 export const DateInput: FC = (props) => {
 
-    const yearRef = useRef(null)
-    const monthRef = useRef(null)
-    const dateRef = useRef(null)
+    const yearRef = useRef<HTMLInputElement>(null)
+    const monthRef = useRef<HTMLInputElement>(null)
+    const dateRef = useRef<HTMLInputElement>(null)
+
+    const [year, setYear] = useState('')
+    const [month, setMonth] = useState('')
+    const [date, setDate] = useState('')
+
+    const onYearInput = (value: string) => {
+        const int = parseInt(value)
+        if (int.toString() !== value) {return}
+
+        setYear(value)
+
+        monthRef.current?.focus()
+    }
+
+    const onYearKeyDown = (key: string) => {
+        if (key === 'ArrowRight') {
+            monthRef.current?.focus()
+
+            return
+        }
+
+        if (key === 'ArrowLeft') {
+            dateRef.current?.focus()
+
+            return
+        }
+    }
+
+    const onMonthInput = (value: string) => {
+        const int = parseInt(value)
+        if (int.toString() !== value) {return}
+
+        setMonth(value)
+
+        dateRef.current?.focus()
+    }
+
+    const onMonthKeyDown = (key: string) => {
+        if (key === 'ArrowRight') {
+            dateRef.current?.focus()
+
+            return
+        }
+
+        if (key === 'ArrowLeft') {
+            yearRef.current?.focus()
+
+            return
+        }
+    }
+
+    const onDateInput = (value: string) => {
+        const int = parseInt(value)
+        if (int.toString() !== value) {return}
+
+        setDate(value)
+
+        yearRef.current?.focus()
+    }
 
     return (
     <Frame>
 
         <input
+            onChange={({target}) => onYearInput(target.value)}
+            value={year}
+            onKeyDown={({key}) => onYearKeyDown(key)}
             ref={yearRef}
             type={'text'}
             pattern={'[0-9]*'}
@@ -19,7 +81,10 @@ export const DateInput: FC = (props) => {
         <div>/</div>
 
         <input
+            onChange={({target}) => onMonthInput(target.value)}
+            value={month}
             ref={monthRef}
+            onKeyDown={({key}) => onMonthKeyDown(key)}
             type={'text'}
             pattern={'[0-9]*'}
             placeholder={'MM'}/>
@@ -27,6 +92,8 @@ export const DateInput: FC = (props) => {
         <div>/</div>
 
         <input
+            onChange={({target}) => onDateInput(target.value)}
+            value={date}
             ref={dateRef}
             type={'text'}
             pattern={'[0-9]*'}
