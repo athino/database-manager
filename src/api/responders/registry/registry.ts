4d9fb2593596bfc36e13fdcfa3a90b3a179852ab
Johannes Skuterud <johann1301s@icloud.com>
2022-06-22 22:27:37 +0200

    w

--- src/api/responders/registry/registry.ts
+++ src/api/responders/registry/registry.ts
@@ -15,17 +15,34 @@ export const registry = new api.Responder<Input, Output>(async (context) => {
     const {hostname} = urlInfo.getHostname({ req: context.native.req })
     const {scopeName, packageName} = context.pathVariables()
 
+    console.log(`1-${baseUrl}`)
+    console.log(`2-${hostname}`)
+    console.log(`3-${scopeName}`)
+    console.log(`4-${packageName}`)
+
     if (!baseUrl)                     { throw new Error() }
+    console.log(`5`)
+
     if (!hostname)                    { throw new Error() }
+    console.log(`6`)
+
     if (!scopeName)                   { throw new Error() }
+    console.log(`7`)
+
     if (!packageName)                 { throw new Error() }
+    console.log(`8`)
+
     if (scopeName !== hostname)       { throw new Error() }
+    console.log(`9`)
+
 
     const {payload} = await database.getPackument({
         baseUrl: baseUrl,
         scopeName: scopeName,
         databaseName: packageName
     })
+
+    console.log(`10-${JSON.stringify(payload)}`)
 
     context.native.res.json(payload?.packument)
 })
