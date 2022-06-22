b5332ce6a70b5f3bd713886a4fb702be2fcc7e71
Johannes Skuterud <johann1301s@icloud.com>
2022-06-22 18:04:33 +0200

    w

--- src/urlinfo/getHostname.ts
+++ src/urlinfo/getHostname.ts
@@ -3,7 +3,7 @@ import {NextApiRequest} from "next"
 export const getHostname = (arg: {
     req: NextApiRequest
 }) => {
-    const hostname = arg.req.headers.host?.split(':').at(0)
+    const hostname = arg.req.headers.host?.split(':').at(0)?.split('.').at(0)
 
     return {
         hostname: hostname
