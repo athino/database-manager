43f175545481e72d8ce9a7bb5798f6fade1de502
Johannes Skuterud <johann1301s@icloud.com>
2022-06-27 20:16:18 +0200

    w

--- src/tarballsource/tarballSource.ts
+++ src/tarballsource/tarballSource.ts
@@ -1,8 +1,31 @@
 
+const buildPath = (arg: {
+    scopeName: string
+    packageName: string
+    databaseName: string
+    major: number
+    minor: number
+    patch: number
+}) => {
+    const posMajor = arg.major < 0 ? -arg.major : arg.major
+    const posMinor = arg.minor < 0 ? -arg.minor : arg.minor
+    const posPatch = arg.patch < 0 ? -arg.patch : arg.patch
+    const a = arg.scopeName
+    const b = arg.databaseName
+    const c = arg.databaseName
+    const d = posMajor
+    const e = posMinor
+    const f = posPatch
+
+    return {
+        path: `/api/tarball/@${a}/${b}/-/${c}-${d}.${e}.${f}.tgz`
+    }
+}
 
 export const tarballSource = {
     staticPath: '/api/tarball/:scopeName/:packageName/-/:databaseName-:major(\\d+).:minor(\\d+).:patch(\\d+).tgz' as const,
-    buildUrl: (arg: {
+    buildPath,
+    buildUrl: ({baseUrl, ...arg}: {
         baseUrl: string
         scopeName: string
         packageName: string
@@ -11,20 +34,10 @@ export const tarballSource = {
         minor: number
         patch: number
     }) => {
-        const posMajor = arg.major < 0 ? -arg.major : arg.major
-        const posMinor = arg.minor < 0 ? -arg.minor : arg.minor
-        const posPatch = arg.patch < 0 ? -arg.patch : arg.patch
-        const a = arg.scopeName
-        const b = arg.databaseName
-        const c = arg.databaseName
-        const d = posMajor
-        const e = posMinor
-        const f = posPatch
-
-        const path = `/api/tarball/@${a}/${b}/-/${c}-${d}.${e}.${f}.tgz`
+        const {path} = buildPath(arg)
 
         return {
-            url: `${arg.baseUrl}${path}`
+            url: `${baseUrl}${path}`
         }
     }
 }
