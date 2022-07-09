3422a540475025318dc2c57357d9d32cb3c441b4
Johannes Skuterud <johann1301s@icloud.com>
2022-07-10 00:35:07 +0200

    w

--- pages/tooltip-example.tsx
+++ pages/tooltip-example.tsx
@@ -4,13 +4,25 @@ import {Tooltip} from "common/components/tooltip"
 export default function TooltipExample() {
 
     return (
-        <Tooltip
-            padding={'10px'}
-            margin={'20px 30px 10px 50px'}
-            arrow={'bottom'}>
-            Hei! <br/><br/>
+        <>
+            <Tooltip
+                padding={'10px'}
+                margin={'20px 30px 0 50px'}
+                arrow={'bottom'}>
+                Hei! <br/><br/>
 
-            Dette er en tooltip...
-        </Tooltip>
+                Dette er en tooltip...
+            </Tooltip>
+
+            <Tooltip
+                padding={'10px'}
+                margin={'0 30px 10px 50px'}
+                arrow={'bottom'}>
+                Hei! <br/><br/>
+
+                Dette er en tooltip...
+            </Tooltip>
+
+        </>
     )
 }
