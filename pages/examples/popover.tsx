42871e330ddb5db63e415574b00a9f4e292c15d5
Johannes Skuterud <johann1301s@icloud.com>
2022-08-13 22:12:12 +0200

    w

--- pages/examples/popover.tsx
+++ pages/examples/popover.tsx
@@ -1,7 +1,6 @@
-import { Button } from 'common/components/button'
+import {Button} from 'common/components/button'
 import {Popover} from 'common/components/popover/popover'
 import React from 'react'
-import styled from 'styled-components'
 
 export default function TooltipExample() {
 
@@ -11,7 +10,7 @@ export default function TooltipExample() {
                 margin={'20px'}
                 escape={5}
                 layer={5}
-                target={<Button>Select database</Button>}
+                target={<Button>Select database gy gyug guyg u</Button>}
                 content={'h yg ygygu ef'.repeat(10)}/>
         </>
     )
